d8e511eb25bcb2cfa3cbe44bfb32ccb4071756f4
Replit user <>
2022-09-19 09:27:04 -0500

    Added functionality to handle the card clicks of employees

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -93,17 +93,14 @@ function Employees() {
   const handleTeamSelectionChange = (event) => {
     setSelectedTeam(event.target.value);
   }
-  const handleEmployeeCardClick = (event) => {
-    const transformedEmployees = employees.map((employee)=> {
-      return (employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam ? {...employee, teamName: ''}:
-                                                                  {...employee, teamName: selectedTeam}): employee)
-    })
-    // return transformedEmployees
+  const handleEmployeeCardClick =(event) => {
+    
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+      ? (employee.teamName === selectedTeam) ? { ...employee, teamName: '' } : { ...employee, teamName: selectedTeam }
+      : employee);
+    setEmployees(transformedEmployees);
+
   }
-  //   const transformedEmployees = employees.map((employee) => {
-  //     employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam) ? {...employee, teamName: '' } : { ...employee, teamName: selectedTeam }: employee)
-  //   })
-  // }
 
   return (
     <main>
@@ -122,7 +119,7 @@ function Employees() {
             <div className='card-collection'>
               {employees.map((employee) => {
                 return (
-                  <div id={employee.id} className='standout' style={{ cursor: "pointer" }} key={employee.id} onClick={handleEmployeeCardClick} >
+                  <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} key={employee.id} onClick={handleEmployeeCardClick} >
                     <img src={employee.gender === 'male' ? maleProfile : femaleProfile} alt={employee.fullName} className='card-img-top' />
                     <div className='card-body'>
                       <h5 className='card-title'>
